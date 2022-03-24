7eb5bf01f169843866dd7cbfc3e6547e85e3df41
Stanislav <77130336+standbit@users.noreply.github.com>
2022-03-24 15:25:17 +0300

    Download xkcd-comic about python

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from urllib.parse import urlparse
+from save_comic import download_comic
+from os.path import splitext
+
+
+def get_file_extension(link):
+    link_path = urlparse(link).path
+    extension = splitext(link_path)[-1]
+    return extension
+
+
+def fetch_comic():
+    url = "https://xkcd.com/353/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    converted_response = response.json()
+    comic_link = converted_response["img"]
+    comic_name = converted_response["safe_title"]
+    extension = get_file_extension(comic_link)
+    filename = f"{comic_name}{extension}"
+    download_comic(comic_link, filename)
+
+
+def main():
+    try:
+        fetch_comic()
+    except requests.exceptions.HTTPError as err:
+        print("General Error, incorrect link\n", str(err))
+    except requests.ConnectionError as err:
+        print("Connection Error. Check Internet connection.\n", str(err))
+
+
+if __name__ == "__main__":
+    main()
